cf7294aaa693ee5834f134659bbf10d223f3e0c6
Meghan McEneaney <mceneam1@tcnj.edu>
2016-04-18 02:06:33 -0400

    updated UI

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,9 +10,6 @@ Bundler.require(*Rails.groups)
 module WeatherApp
   class Application < Rails::Application
 
-    
-
-   
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
